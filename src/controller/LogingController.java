162dcebcd829312237e5254d572f34899da6bd2f
Roarsita <adrodas@scl.edu.gt>
2024-09-17 17:24:49 -0600

    controller

--- src/controller/LogingController.java
+++ src/controller/LogingController.java
@@ -1,4 +1,16 @@
 package controller;
 
+import view.LoginView;
+
 public class LogingController {
+    private LoginView loginView;
+
+    public LogingController(LoginView loginView){
+        this.loginView=loginView;
+        //this.loginView.addLog(e--> hanleLogin());
+
+    }
+    public void controllerDeLogin(){
+        System.out.println("HOLI");
+    }
 }
--- src/view/LoginView.java
+++ src/view/LoginView.java
@@ -1,38 +1,52 @@
 package view;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionListener;
+import java.util.EventListener;
 
 public class LoginView extends JFrame {
+
+    private JLabel passwordLabel;
+    private JLabel emailField;
+    private JButton loginButton;
+
     public LoginView() {
         setTitle("Formulario de Inicio de Sesión");
         setSize(400, 700);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLocationRelativeTo(null); // Centra la ventana en la pantalla
-        setLayout(null); // Deshabilita el layout manager predeterminado
+        setLocationRelativeTo(null);
+        setLayout(null);
 
-        // Crear y configurar los componentes
+
         JLabel emailLabel = new JLabel("Nombre/Correo:");
-        emailLabel.setBounds(20, 300, 120, 30); // x, y, width, height
+        emailLabel.setBounds(20, 300, 120, 30);
         add(emailLabel);
 
         JTextField emailField = new JTextField(20);
-        emailField.setBounds(150, 300, 200, 30); // x, y, width, height
+        emailField.setBounds(150, 300, 200, 30);
         add(emailField);
 
         JLabel passwordLabel = new JLabel("Contraseña:");
-        passwordLabel.setBounds(20, 350, 120, 30); // x, y, width, height
+        passwordLabel.setBounds(20, 350, 120, 30);
         add(passwordLabel);
 
         JPasswordField passwordField = new JPasswordField(20);
-        passwordField.setBounds(150, 350, 200, 30); // x, y, width, height
+        passwordField.setBounds(150, 350, 200, 30);
         add(passwordField);
 
         JButton loginButton = new JButton("Iniciar Sesión");
-        loginButton.setBounds(150, 400, 150, 30); // x, y, width, height
-        add(loginButton);
+        loginButton.setBounds(150, 400, 150, 30);
 
     setVisible(true);
     }
-
+public String getCorreo(){
+        return emailField.getText();
+}
+public String getContra(){
+        return new String(passwordLabel.getText());
+}
+public void evento(EventListener elister){
+        loginButton.addActionListener((ActionListener) elister);
+}
 }
 
