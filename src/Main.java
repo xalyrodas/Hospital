38b47e72040e51671eeb854560e9a4209e5784d2
Roarsita <adrodas@scl.edu.gt>
2024-09-17 22:46:02 -0600

    diseño terminado

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,12 @@
+import view.DoctorView;
+
 import javax.swing.*;
 import java.awt.*;
 
 public class Main {
 
     public static void main(String[] args) {
-
+        new DoctorView();
 
     }
 }
